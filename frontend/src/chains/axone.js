d74e1e0c3883a4c03ba6b869f78fee224385b480
ccamel <camel.christophe@gmail.com>
2025-03-04 09:32:43 +0100

    fix: switch to JayJay validator node endpoints for axone-dentrite-1

--- frontend/src/chains/axone.js
+++ frontend/src/chains/axone.js
@@ -1,8 +1,8 @@
 export const axoneChain = {
   chainId: 'axone-1',
   chainName: 'Axone',
-  rpc: 'https://rpc.axone.network',
-  rest: 'https://lcd.axone.network',
+  rpc: 'https://axone-rpc.jayjayinfra.space/',
+  rest: 'https://axone-api.jayjayinfra.space/',
   stakeCurrency: {
     coinDenom: 'AXONE',
     coinMinimalDenom: 'uaxone',
